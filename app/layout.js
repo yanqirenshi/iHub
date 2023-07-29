915b947e654046f10db027180bc3945ec84301ed
yanqirenshi <yanqirenshi@gmail.com>
2023-07-29 11:36:45 +0900

    fix details

--- app/layout.js
+++ app/layout.js
@@ -34,6 +34,7 @@ const account_menu = [
     { url: '/system-environment', label: 'システム構成' },
     { url: '/message',            label: 'メッセージ' },
     { url: '/wbs',                label: 'WBS' },
+    { url: '/scrum',              label: 'Scrum' },
     { url: '/account',            label: 'アカウント' },
     { url: '/auth',               label: '認証' },
     { url: '/next.js',            label: 'Next.js' },
--- assemblies/Account.js
+++ assemblies/Account.js
@@ -1,4 +1,3 @@
-'use client';
 import * as React from 'react';
 import Avatar from './Account/Avater.js';
 import Menu from './Account/Menu.js';
