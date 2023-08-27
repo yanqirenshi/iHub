2d95b9ab64ef449451dc1fe05ce70f9daf697314
yanqirenshi <yanqirenshi@gmail.com>
2023-08-27 16:58:19 +0900

    add contents of SystemConfigurationDiagram

--- src/pages/SystemEnvironment.js
+++ src/pages/SystemEnvironment.js
@@ -4,6 +4,8 @@ import { useRecoilState } from "recoil";
 import * as atoms from '../recoil/ATOMS.js';
 
 import Frame from '../frames/FrameTabs.js';
+import Wbs from '../panels/Wbs.js';
+import SystemConfigurationDiagram from '../panels/scd/SystemConfigurationDiagram.js';
 
 export default function SystemEnvironment () {
     const [page_data, setPageData] = useRecoilState(atoms.PAGE_SYSTEMS);
@@ -21,9 +23,9 @@ export default function SystemEnvironment () {
         <Frame tabs={tabs}
                onChangeTabs={changeTabs}>
 
-          {'system-configuration-diagram'===tab && <div/>}
-          {'wbs'===tab && <div/>}
-          {'help'===tab && <div/>}
+          {'system-configuration-diagram'===tab && <SystemConfigurationDiagram/>}
+          {'wbs'===tab && <Wbs/>}
+          {'help'===tab && <div>Help....</div>}
         </Frame>
     );
 }
---
+++ src/panels/scd/SystemConfigurationDiagram.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+
+import D3Deployment, { Rectum } from '@yanqirenshi/d3.deployment';
+
+import DEPLOYMENT_MANAGE_OVERVIEW_EDGE_DATA from '../../data/samples/scd/DEPLOYMENT_MANAGE_OVERVIEW_EDGE_DATA.js';
+import DEPLOYMENT_MANAGE_OVERVIEW_NODE_DATA from '../../data/samples/scd/DEPLOYMENT_MANAGE_OVERVIEW_NODE_DATA.js';
+
+const style = {
+    width: '100%',
+    height: '100%',
+};
+
+export default function SystemConfigurationDiagram (props) {
+    const graph_data = {
+        nodes: DEPLOYMENT_MANAGE_OVERVIEW_NODE_DATA,
+        edges: DEPLOYMENT_MANAGE_OVERVIEW_EDGE_DATA,
+    };
+
+    const [rectum] = React.useState(new Rectum({
+        transform: {
+            k: 0.3,
+            x: 1800.0,
+            y: 700.0,
+        },
+    }));
+
+    React.useEffect(()=>{
+        rectum.data(graph_data);
+    }, [rectum, graph_data]);
+
+    return (
+        <Box style={style}>
+          <D3Deployment rectum={rectum} />
+        </Box>
+    );
+}
