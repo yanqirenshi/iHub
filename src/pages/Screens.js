bc1b6996c0868f4581bb90485bfc8bea4f43a726
yanqirenshi <yanqirenshi@gmail.com>
2023-08-27 16:14:54 +0900

    add contents of ScreenTransitionDiagram

--- src/pages/Screens.js
+++ src/pages/Screens.js
@@ -4,6 +4,8 @@ import { useRecoilState } from "recoil";
 import * as atoms from '../recoil/ATOMS.js';
 
 import Frame from '../frames/FrameTabs.js';
+import Wbs from '../panels/Wbs.js';
+import ScreenTransitionDiagram from '../panels/std/ScreenTransitionDiagram.js';
 
 export default function Screens () {
     const [page_data, setPageData] = useRecoilState(atoms.PAGE_SCREENS);
@@ -21,9 +23,9 @@ export default function Screens () {
         <Frame tabs={tabs}
                onChangeTabs={changeTabs}>
 
-          {'screen-transition-diagram'===tab && <div/>}
-          {'wbs'===tab && <div/>}
-          {'help'===tab && <div/>}
+          {'screen-transition-diagram'===tab && <ScreenTransitionDiagram/>}
+          {'wbs'===tab                       && <Wbs/>}
+          {'help'===tab                      && <div>Help.....</div>}
         </Frame>
     );
 }
---
+++ src/panels/std/ScreenTransitionDiagram.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+import Box from '@mui/material/Box';
+
+import D3Sitemap, { Rectum } from '@yanqirenshi/d3.sitemap';
+
+import SITEMAP_DATA from '../../data/samples/SITEMAP_DATA.js';
+
+const style = {
+    background: '#f3f3f3',
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+};
+
+// <SitemapGraph source={SITEMAP_DATA}
+//               x={1800}
+//               y={-200}
+//               scale={0.5}/>
+
+export default function ScreenTransitionDiagram (props) {
+    const source= SITEMAP_DATA;
+
+    const [rectum] = useState(new Rectum({
+        transform:  {
+            k: props.scale || 1.0,
+            x: props.x || 0.0,
+            y: props.y || 0.0,
+        },
+    }));
+
+    const [graph_data, setGraphData] = useState(null);
+
+    useEffect(()=> setGraphData(source), [source]);
+
+    if (graph_data && !rectum.data())
+        rectum.data(graph_data);
+
+    if (!graph_data && !rectum.data()) return null;
+
+    return (
+        <Box sx={style}>
+          <D3Sitemap rectum={rectum} />
+        </Box>
+    );
+}
