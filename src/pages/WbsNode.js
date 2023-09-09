41781788d9f9583ef2ac7b27772f1edf425a838d
yanqirenshi <yanqirenshi@gmail.com>
2023-09-09 14:31:07 +0900

    add lazy

--- src/pages/WbsNode.js
+++ src/pages/WbsNode.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+
 import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import S from '@mui/material/Typography';
@@ -6,19 +7,16 @@ import S from '@mui/material/Typography';
 import Frame from '../frames/Frame.js';
 import Container from '@mui/material/Container';
 
-// lazy loading
-// https://qiita.com/kakken1988/items/9f14480c8a9f0abf7b3b
+// TODO: あと、ページ無いときの処理とかを入れる。
+// https://qiita.com/kakken1988/items/9f14480c8a9f0abf7b3b#chunkloaderror-%E3%81%AE%E5%9B%9E%E9%81%BF%E7%AD%96
 export default function WbsNode () {
     const {wbs_id} = useParams();
 
+    const Page = lazy(() => import(`./wbs/P${wbs_id}.js`));
+
     return (
-        <Frame>
-          <Container maxWidth="lg" sx={{pt:5}}>
-            <Box>
-              <S>WBS Node</S>
-              <S>{wbs_id}</S>
-            </Box>
-          </Container>
-        </Frame>
+        <Suspense>
+          <Page/>
+        </Suspense>
     );
 }
--- src/pages/wbs/P1000062.js
+++ src/pages/wbs/P1000062.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import S from '@mui/material/Typography';
 
-import Frame from '../frames/Frame.js';
+import Frame from '../../frames/Frame.js';
 
-import Container from '@mui/material/Container';
-
-export default function P1000062 () {
-  return (
-      <Frame>
+export default function P1000062 (props) {
+    return (
+        <Frame>
           <Container maxWidth="lg" sx={{pt:5}}>
             <Box>
               <S>1000062</S>
             </Box>
           </Container>
-      </Frame>
-  );
+        </Frame>
+    );
 }
