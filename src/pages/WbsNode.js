eb241daaea1fe446e2e16e45efe7c9ac3d6f3562
yanqirenshi <yanqirenshi@gmail.com>
2023-09-09 14:38:07 +0900

    add fallback

--- src/pages/WbsNode.js
+++ src/pages/WbsNode.js
@@ -3,9 +3,10 @@ import React, { lazy, Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import S from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 import Frame from '../frames/Frame.js';
-import Container from '@mui/material/Container';
+import Loading from '../panels/Loading.js';
 
 // TODO: あと、ページ無いときの処理とかを入れる。
 // https://qiita.com/kakken1988/items/9f14480c8a9f0abf7b3b#chunkloaderror-%E3%81%AE%E5%9B%9E%E9%81%BF%E7%AD%96
@@ -15,7 +16,7 @@ export default function WbsNode () {
     const Page = lazy(() => import(`./wbs/P${wbs_id}.js`));
 
     return (
-        <Suspense>
+        <Suspense fallback={<Loading/>}>
           <Page/>
         </Suspense>
     );
