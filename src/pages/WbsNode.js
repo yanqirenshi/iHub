af4a51d0861f40498003b061daf0dd32bdca0693
yanqirenshi <yanqirenshi@gmail.com>
2023-09-09 13:51:59 +0900

    add wbs_id

--- src/pages/WbsNode.js
+++ src/pages/WbsNode.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import S from '@mui/material/Typography';
 
@@ -7,15 +8,17 @@ import Container from '@mui/material/Container';
 
 // lazy loading
 // https://qiita.com/kakken1988/items/9f14480c8a9f0abf7b3b
-
 export default function WbsNode () {
-  return (
-      <Frame>
+    const {wbs_id} = useParams();
+
+    return (
+        <Frame>
           <Container maxWidth="lg" sx={{pt:5}}>
             <Box>
               <S>WBS Node</S>
+              <S>{wbs_id}</S>
             </Box>
           </Container>
-      </Frame>
-  );
+        </Frame>
+    );
 }
