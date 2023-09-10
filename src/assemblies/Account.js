ab6f960ca2fb99f9defbb7c5afa3c90755e9a896
yanqirenshi <yanqirenshi@gmail.com>
2023-09-10 10:00:24 +0900

    add index

--- src/assemblies/Account.js
+++ src/assemblies/Account.js
@@ -5,9 +5,13 @@ import Menu from './Account/Menu.js';
 export default function Account (props) {
     const menu = props.menu;
     const actions = props.actions;
-
     return (
         <>
+          <div style={{position: 'fixed', right: 22, top: 22}}>
+            <p>avater.clicked = {menu.avater.clicked ? 't' : 'f'}</p>
+            <p>avater.enterd  = {menu.avater.enterd  ? 't' : 'f'}</p>
+            <p>menu.enterd    = {menu.menu.enterd    ? 't' : 'f'}</p>
+          </div>
           <Avatar menu={menu} actions={actions}/>
           {isOpenMenu(menu) &&
            <Menu menu={menu} actions={actions}/>}
